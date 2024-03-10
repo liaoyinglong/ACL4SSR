ed5f4e443b029029b8d20d41488d7d409940bdd2
liaoyinglong <vigossliao@gmail.com>
2024-03-10 15:50:34 +0800

    批量上传所有

--- script/upload-to-wrt.js
+++ script/upload-to-wrt.js
@@ -1,8 +1,8 @@
-import { $, path, fs } from "zx";
+import { $, path, fs, globby } from "zx";
 
 const time = new Date().toISOString().split("T")[0];
 
-export async function uploadToWrt(filePath) {
+export async function uploadToWrt(filePath, shouldActive = false) {
   // 获取到最后一级文件名
   const filename = path.basename(filePath);
 
@@ -11,26 +11,41 @@ export async function uploadToWrt(filePath) {
   await $`scp ${filePath} x-wrt:${remotePath}`;
   await $`scp ${filePath} x-wrt:/etc/openclash/backup/${filename}`;
 
-  const commands = [
-    // 设置 openclash 的配置文件路径
-    `uci set openclash.config.config_path=${remotePath}`,
-    // 提交配置
-    `uci commit openclash`,
-    // 重启 openclash
-    `/etc/init.d/openclash restart`,
-  ].join(" && ");
+  if (shouldActive) {
+    const commands = [
+      // 设置 openclash 的配置文件路径
+      `uci set openclash.config.config_path=${remotePath}`,
+      // 提交配置
+      `uci commit openclash`,
+      // 重启 openclash
+      `/etc/init.d/openclash restart`,
+    ].join(" && ");
 
-  await $`ssh x-wrt ${commands}`;
+    await $`ssh x-wrt ${commands}`;
+  }
 }
 
-uploadToWrt(
-  path.join(
-    import.meta.dirname,
-    "config",
-    //
-    time,
-    //
+const dir = path.join(import.meta.dirname, "config", time);
+
+const files = globby.globbySync("**/*.yaml", {
+  cwd: dir,
+  absolute: true,
+});
+
+const active = path.join(
+  dir,
+  //
     "ss-sub.yaml"
-  )
+  //"三分机场-sub.yaml"
+  //''
 );
+
+for (const file of files) {
+  await uploadToWrt(
+    file,
+
+    file === active
+  );
+}
+
 // console.log(import.meta.dirname);
